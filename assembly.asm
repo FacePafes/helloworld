6e387d19a734bd53da7212dab8eb381d1c3a2994
F  A  C  E    P  A  F  E  S <forever_rind6o@icloud.com>
2026-01-27 21:10:05 +0000

    Add assembly code to print 'Hello World!'

---
+++ assembly.asm
@@ -0,0 +1,15 @@
+section .data
+    hello db 'Hello World!', 0
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hello
+    mov rdx, 12
+    syscall
+    mov rax, 60
+    xor rdi, rdi
+    syscall
