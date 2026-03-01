c8c5b3bba0304692cec10d96a54f2226886e4896
F  A  C  E    P  A  F  E  S <forever_rind6o@icloud.com>
2026-03-01 17:58:40 +0000

    Create vhdl.vhd

---
+++ vhdl.vhd
@@ -0,0 +1,11 @@
+entity hello_world is
+end hello_world;
+
+architecture behavior of hello_world is
+begin
+    process
+    begin
+        report "Hello World!";
+        wait;
+    end process;
+end behavior;
